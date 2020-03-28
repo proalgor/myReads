f32d3338b90c647fa7431a3873f7e0a7034406c8
francis <onyishif@gmail.com>
2020-03-29 00:11:35 +0100

    update the search component with the proper view

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -4,8 +4,18 @@ class SearchBooks extends Component{
 
     render() {
         return (
-            <div>
-                Search
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <button className="close-search" onClick={}>Close</button>
+                    <div className="search-books-input-wrapper">
+
+                        <input type="text" placeholder="Search by title or author"/>
+
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid"></ol>
+                </div>
             </div>
         );
     }
