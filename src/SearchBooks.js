01451a4595ed023eef958ad13fc0e97e01c575d0
francis <onyishif@gmail.com>
2020-03-29 00:07:31 +0100

    add the searchbook component

---
+++ src/SearchBooks.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class SearchBooks extends Component{
+
+    render() {
+        return (
+            <div>
+                Search
+            </div>
+        );
+    }
+}
