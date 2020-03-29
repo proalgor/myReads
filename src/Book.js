f65f68aea39a7c04cef1edf065ee77196ed2ffc4
francis <onyishif@gmail.com>
2020-03-30 00:10:23 +0100

    set the value of the select element to the value of the shel the default value will be none

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,7 @@ class Book extends Component{
                     <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                         <div className="book-shelf-changer">
-                            <select>
+                            <select onMouseOver={(element)=>book.shelf ? element.target.value = book.shelf : element.target.value='none'}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
