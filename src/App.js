8c52ae5e13b31c49d569b7a1482fa81594e49e0a
francis <onyishif@gmail.com>
2020-03-30 00:07:42 +0100

    get the books after mounting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import {Route} from 'react-router-dom';
 import Home from './Home'
 import SearchBooks from "./SearchBooks";
@@ -7,20 +7,27 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+
+    books:[]
+
   }
 
-  render() {
+  componentDidMount() {
+    BooksAPI.getAll()
+        .then((result)=>{
+            this.setState(()=>({
+                books:result
+            }))
+        })
+  }
+
+    render() {
     return (
         <div>
         <Route exact path='/'
-               component={Home}
+               render={()=>(
+                   <Home books={this.state.books}/>
+               )}
         />
         <Route path='/search'
                component={SearchBooks}
