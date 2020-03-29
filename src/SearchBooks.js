199254dba622b4df1927434ba4c5720ecb935582
francis <onyishif@gmail.com>
2020-03-29 23:11:55 +0100

    update the book component with data

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,21 +1,39 @@
 import React,{Component} from 'react';
 import {Link} from "react-router-dom";
+import * as BooksAPI from './BooksAPI'
+import Book from './Book';
+
 
 class SearchBooks extends Component{
-
+    state = {
+        books:[]
+    }
+    search = (query) => {
+        BooksAPI.search(query)
+            .then((result)=> this.setState(()=>({
+                books: result,
+            }))
+            )
+    };
     render() {
+        const {books}= this.state
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link to='/' className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
 
-                        <input type="text" placeholder="Search by title or author"/>
+                        <input type="text" placeholder="Search by title or author"
+                                onChange={(event)=>this.search(event.target.value)}
+                        />
 
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {this.state.books!==undefined && books.map((book,index)=>(<Book key={index} book={book}/>))}
+                        {console.log(this.state.books)}
+                    </ol>
                 </div>
             </div>
         );
